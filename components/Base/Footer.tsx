178e34d18d21a55e644d41c50fe42291b78fac21
Nicholas LaMothe <lamothe.dev@gmail.com>
2023-11-25 00:35:08 -0800

    removed commented code

--- components/Base/Footer.tsx
+++ components/Base/Footer.tsx
@@ -1,15 +1,3 @@
-// export default function Footer() {
-//   return (
-//     <footer>
-//       <a href='https://www.linkedin.com/in/nicholas-lamothe/' target='_blank'>
-//         <img src='img/linkedin_logo.png' alt='' />
-//       </a>
-//       <a href='https://github.com/nlamo/' target='_blank'>
-//         <img src='img/github_logo.png' alt='' />
-//       </a>
-//     </footer>
-//   )
-// }
 import { Container, Group, ActionIcon, rem } from '@mantine/core';
 import { IconBrandGithub, IconBrandLinkedin } from '@tabler/icons-react';
 import classes from './_Base.module.scss';
